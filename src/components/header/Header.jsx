53d4694a590704c377c0981325b8dd8d801edf30
Daniel-Avramovic <danijelgti@gmail.com>
2021-08-20 22:00:14 +0200

    remove button and add a tag

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({changeUi, view}) => {
     return(
         <header>
         <h1>React User</h1>
-        <button onClick={changeUi} type="button" className="btn btn-primary btn-sm"> <i className="medium material-icons">{view? "grid_on" : "format_list_bulleted"}</i></button>
+        <a href='!#' onClick={changeUi} className="btn-flat disabled"> <i className="medium material-icons">{view? "grid_on" : "format_list_bulleted"}</i></a>
         </header>
     )
 };
