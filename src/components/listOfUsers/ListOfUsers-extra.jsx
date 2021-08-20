86c661c8b545f0d253d5aa70f41cda9b08598724
Daniel-Avramovic <danijelgti@gmail.com>
2021-08-20 22:29:48 +0200

    update card and css

--- src/components/listOfUsers/ListOfUsers-extra.jsx
+++ src/components/listOfUsers/ListOfUsers-extra.jsx
@@ -9,7 +9,8 @@ export const ListOfUsersExtra = ({users}) => {
     <Container>
       <Row>
         {users.map((result, index) => (
-          <div className="col-12 col-md-4 col-sm-6 aps" key={index}>
+          <div className="col-12 col-md-4 col-sm-6 aps mb-3" key={index}>
+            <div className="card shadow">
             <img
               src={result.picture.large}
               alt="Loading!!!"
@@ -19,6 +20,7 @@ export const ListOfUsersExtra = ({users}) => {
               <p className="name">{result.name.first}</p>
               <p>E-mail: {hideAdress(result.email)}</p>
               <p>Date of birth: {parseDateExtra(result.dob.date)}</p>
+            </div>
             </div>
           </div>
         ))}
