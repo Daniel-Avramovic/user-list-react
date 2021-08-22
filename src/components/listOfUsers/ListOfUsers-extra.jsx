733223f6c409e1cbf655eb364327e920886ee501
Daniel-Avramovic <danijelgti@gmail.com>
2021-08-22 21:10:21 +0200

    add class for female users at grid view

--- src/components/listOfUsers/ListOfUsers-extra.jsx
+++ src/components/listOfUsers/ListOfUsers-extra.jsx
@@ -16,22 +16,28 @@ export const ListOfUsersExtra = ({ users, val }) => {
     <Container>
       <p className="count">{count(filterUser(users, val))}</p>
       <Row>
-        {filterUser(users, val).map((result, index) => (
+        {filterUser(users, val).map((user, index) => {
+           let cardClass = "card shadow";
+           if (user.gender === "female") {
+            cardClass = "card shadow purpleClass";
+          }
+          return (
           <div className="col-12 col-md-4 col-sm-6 aps mb-3" key={index}>
-            <div className="card shadow">
+            <div className={cardClass}>
               <img
-                src={result.picture.large}
+                src={user.picture.large}
                 alt="Loading!!!"
                 className="imgExtra"
               />
               <div>
-                <p className="name">{result.name.first}</p>
-                <p>E-mail: {hideAdress(result.email)}</p>
-                <p>Date of birth: {parseDateExtra(result.dob.date)}</p>
+                <p className="name">{user.name.first}</p>
+                <p>E-mail: {hideAdress(user.email)}</p>
+                <p>Date of birth: {parseDateExtra(user.dob.date)}</p>
               </div>
             </div>
           </div>
-        ))}
+          )
+          })}
       </Row>
     </Container>
   );
--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const SearchBar = (props) => {
 console.log(props, 'value')
     return(
+        <div className="container">
+        <i class="material-icons prefix">search</i>
         <input type="search" placeholder="Search" value={props.val} onChange={props.search}/>
+        </div>
     )
 }
 
